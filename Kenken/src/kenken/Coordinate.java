7a5e53f8b081b926814391b4c80bac0a784cca3b
Armando <marcasro9@gmail.com>
2018-04-19 22:25:13 -0600

    -Create class figure -Create class coordinate

---
+++ Kenken/src/kenken/Coordinate.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kenken;
+
+/**
+ *
+ * @author Armando
+ */
+public class Coordinate {
+    int x=0;
+    int y=0;
+    
+    public void Coordinate(){
+        x=0;
+        y=0;
+    }
+}
