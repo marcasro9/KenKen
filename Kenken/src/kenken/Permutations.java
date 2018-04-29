98b381b0cbec5c5396d8350f9fef5cef28d4c046
PC-ISRALIAN\Israel Padilla <isfa98@hotmail.com>
2018-04-29 14:15:12 -0600

    +Permutation algorithm progress 50%

--- Kenken/src/kenken/Permutations.java
+++ Kenken/src/kenken/Permutations.java
@@ -14,36 +14,49 @@ import java.util.List;
  */
 public class Permutations {
     
-    public  static void permute(int start, int[] input ) {
-        if (start == input.length) {
-            //System.out.println(input);
-            for(int x: input){
-            System.out.print(x);
+    public static ArrayList<int[]> permute(int n, int figure){
+        ArrayList<int[]> permutes=new  ArrayList<>(); 
+        if(figure==2){
+               int a=0;
+               int b=n;
+               for(a=0; a<=n;a++){
+                   int [] S={a,n-a};
+                   permutes.add(S);               
+               }
+        }
+        else if(figure>2){
+            int a=0;
+               int b=n;
+               for(a=0; a<=n;a++){
+                    for(int[] c: permute(n-a,figure-1)){
+                        int [] S=new int[figure];
+                        S[0]=a;
+                        for(int i=1;i<figure;i++){
+                           S[i] = c[i-1];
+                        }
+                        permutes.add(S);
+                   }          
+               }
+        }
+        return permutes;
+    }
+    
+    public static void print(ArrayList<int[]> list){
+        for(int[] i:list){
+            for(int x:i){
+                System.out.println(x + " ");
             }
         System.out.println("");
-        return;
-        }
-        for (int i = start; i < input.length; i++) {
-            // swapping
-            int temp = input[i];
-            input[i] = input[start];
-            input[start] = temp;
-            // swap(input[i], input[start]);
-            
-            permute(start + 1, input);
-            // swap(input[i],input[start]);
-
-            int temp2 = input[i];
-            input[i] = input[start];
-            input[start] = temp2;
         }
     }
-
+    
     public static void main(String[] args) {
-
-        int[] a={1,2,3};
-        permute(0, a);    
-}
+        int n=15;
+        int f=2;
+        ArrayList<int[]> prueba=new ArrayList<>();
+        prueba= permute(n,f);
+        print(prueba);
+    }
 
 
 }
